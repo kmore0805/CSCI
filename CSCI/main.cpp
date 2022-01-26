546594b9940fd4bcb7ce9b736f270365b9797d5d
kmore0805 <kevin.moreno1997km@gmail.com>
2022-01-26 11:51:48 -0600

    Initial Commit

---
+++ CSCI/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CSCI
+//
+//  Created by Kevin Moreno on 1/26/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
